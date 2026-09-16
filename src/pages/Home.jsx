128f97aa8024f52bc3d01d7f1e36e08195a98f17
Carlos Navarro Jr <cnavarrojr22@gmail.com>
2026-09-15 20:13:36 -0500

    Added more components and also added a navbar along with a way to see the favorites page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,20 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+        const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         {id: 1, title: "John Wick", release_date: "2020"},
         {id: 1, title: "Terminator", release_date: "1999"},
         {id: 1, title: "John Wick", release_date: "1998"},
     ];
 
-    const handleSearch = () => {};
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
+    };
 
     return (
         <div className="home">
@@ -16,13 +23,18 @@ function Home() {
                     type="text"
                     placeholder="Search for movies"
                     className="search-input" 
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
                 />
+                <button type="submit" className="search-button">Search</button>
             </form>
 
             <div className="movies-grid">
-                {movies.map((movie) => (
+                {movies.map((movie) => 
+                movie.title.toLowerCase().startsWith(searchQuery) && (
                     <MovieCard movie={movie} key={movie.id} />
-                ))}
+                )
+                )}
             </div>
         </div>
     );
